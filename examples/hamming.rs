8eaadd694ded1cf8d32a065134f0458c227207c0
uzushino <torattoria.mamiya@gmail.com>
2021-06-16 20:44:06 +0900

    fix fmt all

--- examples/hamming.rs
+++ examples/hamming.rs
@@ -1,8 +1,8 @@
 use little_annoy::{Annoy, Hamming};
 
-use std::convert::TryInto;
-use img_hash::HasherConfig;
 use bit_vec::BitVec;
+use img_hash::HasherConfig;
+use std::convert::TryInto;
 
 fn vec_to_fixed_slice<T: Copy + Default, const N: usize>(v: Vec<T>) -> [T; N] {
     v.try_into().unwrap_or_else(|v: Vec<T>| [T::default(); N])
@@ -19,17 +19,29 @@ fn main() {
 
     let image1 = image::open("data/a.jpg").unwrap();
     let image2 = image::open("data/b.jpg").unwrap();
-    
+
     let hasher = HasherConfig::new().to_hasher();
 
     let hash1 = hasher.hash_image(&image1);
     let hash2 = hasher.hash_image(&image2);
 
-    let image1_binary: Vec<u8> = hash1.as_bytes().iter().map(|x| to_binary(*x)).flatten().map(|x| x as u8).collect();
-    let image2_binary: Vec<u8> = hash2.as_bytes().iter().map(|x| to_binary(*x)).flatten().map(|x| x as u8).collect();
+    let image1_binary: Vec<u8> = hash1
+        .as_bytes()
+        .iter()
+        .map(|x| to_binary(*x))
+        .flatten()
+        .map(|x| x as u8)
+        .collect();
+    let image2_binary: Vec<u8> = hash2
+        .as_bytes()
+        .iter()
+        .map(|x| to_binary(*x))
+        .flatten()
+        .map(|x| x as u8)
+        .collect();
     let image1_binary: [f64; 64] = vec_to_fixed_slice(image1_binary) as [f64; 64];
     let image2_binary: [f64; 64] = vec_to_fixed_slice(image2_binary) as [f64; 64];
-    
+
     ann.add_item(1, image1_binary);
     ann.add_item(2, image2_binary);
     ann.add_item(3, image2_binary);
