a1bab0133ca18a637eb0745e5b7ac167c0f024b1
uzushino <torattoria.mamiya@gmail.com>
2021-07-19 19:41:36 +0900

    fix type

--- little_annoy/benches/benchmarks/simple.rs
+++ little_annoy/benches/benchmarks/simple.rs
@@ -4,15 +4,15 @@ use little_annoy::{Annoy, Euclidean};
 use rand;
 
 pub fn build(c: &mut Criterion) {
-    let mut ann: Annoy<f64, Euclidean, 2> = Annoy::new();
+    let mut ann: Annoy<f64, Euclidean> = Annoy::new(2);
 
-    ann.add_item(0, [1.0, 1.0]);
-    ann.add_item(1, [5.0, 5.0]);
-    ann.add_item(2, [2.0, 2.0]);
-    ann.add_item(3, [4.0, 4.0]);
+    ann.add_item(0, &[1.0, 1.0]);
+    ann.add_item(1, &[5.0, 5.0]);
+    ann.add_item(2, &[2.0, 2.0]);
+    ann.add_item(3, &[4.0, 4.0]);
 
     for z in 4..10 {
-        ann.add_item(z, [10.0, 10.0]);
+        ann.add_item(z, &[10.0, 10.0]);
     }
 
     c.bench_function("build 2", |b| b.iter(|| ann.build(2)));
@@ -31,27 +31,27 @@ pub fn add_item(c: &mut Criterion) {
 
     c.bench_function("add_item 2", |b| {
         b.iter(|| {
-            let mut ann: Annoy<f64, Euclidean, 2> = Annoy::new();
+            let mut ann: Annoy<f64, Euclidean> = Annoy::new(2);
             for i in 0..100 {
-                ann.add_item(i, create_item::<2>());
+                ann.add_item(i, &create_item::<2>());
             }
             ann.build(100)
         })
     });
     c.bench_function("add_item 100", |b| {
         b.iter(|| {
-            let mut ann: Annoy<f64, Euclidean, 100> = Annoy::new();
+            let mut ann: Annoy<f64, Euclidean> = Annoy::new(100);
             for i in 0..100 {
-                ann.add_item(i, create_item::<100>());
+                ann.add_item(i, &create_item::<100>());
             }
             ann.build(100)
         })
     });
     c.bench_function("add_item 10_000", |b| {
         b.iter(|| {
-            let mut ann: Annoy<f64, Euclidean, 10_000> = Annoy::new();
+            let mut ann: Annoy<f64, Euclidean> = Annoy::new(10_000);
             for i in 0..100 {
-                ann.add_item(i, create_item::<10_000>());
+                ann.add_item(i, &create_item::<10_000>());
             }
             ann.build(100)
         })
--- little_annoy_wasm/src/lib.rs
+++ little_annoy_wasm/src/lib.rs
@@ -1,6 +1,6 @@
 use little_annoy::{Annoy, Euclidean};
 use wasm_bindgen::prelude::*;
-use web_sys::console::log_1;
+use web_sys::console::*;
 
 #[wasm_bindgen]
 pub fn ann_new(f: u8) -> *mut Annoy<f64, Euclidean> {
@@ -11,7 +11,7 @@ pub fn ann_new(f: u8) -> *mut Annoy<f64, Euclidean> {
 #[wasm_bindgen]
 pub fn build(ann_ptr: *mut Annoy<f64, Euclidean>, n: i64) -> Result<(), JsValue>{
     let ann = unsafe { ann_ptr.as_mut() };
-
+    
     if let Some(ann) = ann {
         ann.build(n);
     }
@@ -22,7 +22,6 @@ pub fn build(ann_ptr: *mut Annoy<f64, Euclidean>, n: i64) -> Result<(), JsValue>
 #[wasm_bindgen]
 pub fn add_item(ann_ptr: *mut Annoy<f64, Euclidean>, idx: u32, v: &[f64]) -> Result<(), JsValue>{
     let ann = unsafe { ann_ptr.as_mut() };
-
     if let Some(ann) = ann {
         ann.add_item(idx as i64, v);
     }
