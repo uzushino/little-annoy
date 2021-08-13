b17b7aaccfd972f1ce7d50cd3a0a63e718285b40
uzushino <torattoria.mamiya@gmail.com>
2021-08-13 23:37:14 +0900

    impl integer

--- little_annoy/src/float.rs
+++ little_annoy/src/float.rs
@@ -9,11 +9,19 @@ pub trait Float:
     + PartialEq
     + PartialOrd
     + Clone
-    + num::traits::real::Real
     + Copy
 {
+    fn sqrt(self) -> Self {
+        let v = Self::to_f32(&self).unwrap();
+        let f = v.sqrt();
+        Self::from_f32(f).unwrap()
+    }
 }
 
 impl Float for f64 {}
 
 impl Float for f32 {}
+
+impl Float for i64 {}
+
+impl Float for i32 {}
