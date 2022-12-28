f63277628cb8db24798dc279a235ddf06cab0a33
uzushino <torattoria.mamiya@gmail.com>
2022-12-28 14:44:56 +0900

    remove comment out

--- little_annoy/examples/save_load.rs
+++ little_annoy/examples/save_load.rs
@@ -8,28 +8,28 @@ fn print_distance(ann: &mut Annoy<f64, Euclidean>) {
 }
 
 fn main() {
-       let mut ann: Annoy<f64, Euclidean> = Annoy::new(2);
+    let mut ann: Annoy<f64, Euclidean> = Annoy::new(2);
 
-       ann.add_item(0, &[1.0, 1.0]);
-       ann.add_item(1, &[5.0, 5.0]);
-       ann.add_item(2, &[2.0, 2.0]);
-       ann.add_item(3, &[4.0, 4.0]);
-       ann.build(1000);
+    ann.add_item(0, &[1.0, 1.0]);
+    ann.add_item(1, &[5.0, 5.0]);
+    ann.add_item(2, &[2.0, 2.0]);
+    ann.add_item(3, &[4.0, 4.0]);
+    ann.build(1000);
 
-       println!("Print distance.");
-       print_distance(&mut ann);
+    println!("Print distance.");
+    print_distance(&mut ann);
 
-       let file = std::fs::File::create("/tmp/hoge.db").expect("Could not create temp file");
-       println!("Save nodes.");
-       let _ = ann.save(file);
+    let file = std::fs::File::create("/tmp/hoge.db").expect("Could not create temp file");
+    println!("Save nodes.");
+    let _ = ann.save(file);
 
-       println!();
+    println!();
 
-       println!("load nodes.");
-       let mut ann: Annoy<f64, Euclidean> = Annoy::new(2);
+    println!("load nodes.");
+    let mut ann: Annoy<f64, Euclidean> = Annoy::new(2);
 
-       let bin = std::fs::File::open("/tmp/hoge.db").unwrap();
-       let _ = ann.load(std::io::BufReader::new(bin));
+    let bin = std::fs::File::open("/tmp/hoge.db").unwrap();
+    let _ = ann.load(std::io::BufReader::new(bin));
 
-       print_distance(&mut ann);
+    print_distance(&mut ann);
 }
