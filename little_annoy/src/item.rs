4f05fe3e34e6b1eb8c028ea950eab77039348808
uzushino <torattoria.mamiya@gmail.com>
2023-01-10 11:14:11 +0900

    move sqrt

--- little_annoy/src/item.rs
+++ little_annoy/src/item.rs
@@ -1,39 +1,83 @@
-use std::fmt::Debug;
+use num::{
+    Zero,
+    One,
+    ToPrimitive,
+    FromPrimitive,
+    traits::NumAssign
+};
 
 pub trait Item:
-    num::Num
-    + num::Zero
-    + num::One
-    + num::traits::NumAssign
-    + num::ToPrimitive
-    + num::FromPrimitive
+    Zero
+    + One
+    + NumAssign
+    + ToPrimitive
+    + FromPrimitive
     + PartialOrd
     + Clone
     + Copy
 {
+    fn sqrt(self) -> Self;
+}
+
+impl Item for isize {
     fn sqrt(self) -> Self {
         let v = Self::to_f64(&self)
             .map(|v| v.sqrt())
             .and_then(Self::from_f64);
-
         v.unwrap_or_else(|| Self::zero())
     }
 }
 
-impl Item for isize {}
+impl Item for i8 {
+    fn sqrt(self) -> Self {
+        let v = Self::to_f64(&self)
+            .map(|v| v.sqrt())
+            .and_then(Self::from_f64);
+        v.unwrap_or_else(|| Self::zero())
+    }
+}
 
-impl Item for i8 {}
+impl Item for i16 {
+    fn sqrt(self) -> Self {
+        let v = Self::to_f64(&self)
+            .map(|v| v.sqrt())
+            .and_then(Self::from_f64);
+        v.unwrap_or_else(|| Self::zero())
+    }
+}
 
-impl Item for i16 {}
+impl Item for f64 {
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+}
 
-impl Item for f64 {}
+impl Item for f32 {
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+}
 
-impl Item for f32 {}
+impl Item for i64 {
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+}
 
-impl Item for i64 {}
+impl Item for i32 {
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+}
 
-impl Item for i32 {}
+impl Item for u64 {
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+}
 
-impl Item for u64 {}
-
-impl Item for u32 {}
+impl Item for u32 {
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+}
